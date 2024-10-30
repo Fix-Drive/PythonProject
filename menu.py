a5ade65e3e7b1edc4adaf97f8adabe3f798491e9
gabisreis7 <gabisousareis07@gmail.com>
2024-10-30 13:26:52 -0300

    Organizando menu de operações

---
+++ menu.py
@@ -0,0 +1,13 @@
+def exibir_menu():
+    while True:
+        print("Menu de Funcionalidades")
+        print("Escolha a opção que deseja efetuar:")
+        print(" 1. Consultar entidade")
+        print("2. Cadastrar objeto")
+        print("3. Atualizar dados")
+        print("4. Deletar dados")
+
+    
+        escolha = int(input("Digite uma opção: "))
+
+        
